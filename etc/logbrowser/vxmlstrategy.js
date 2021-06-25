ed47706b66cfd6a2df044ac5a22901e708c13b14
Stepan Sydoruk <stepan.sydoruk@ext.airbnb.com>
2021-06-25 16:54:32 -0700

    Ignoring some fields for 'expression'. ToDo: filter those out while parsing

--- etc/logbrowser/vxmlstrategy.js
+++ etc/logbrowser/vxmlstrategy.js
@@ -11,7 +11,7 @@ switch (cmd) {
       if (expr.startsWith("Called for")) {
         IGNORE_RECORD = true;
       } else {
-        FIELDS.put("name", cmd + " ->" + m[1]);
+        FIELDS.put("name", cmd + " ->" + expr);
       }
     }
     break;
@@ -20,20 +20,30 @@ switch (cmd) {
   case "expression": {
     var s = RECORD.getBytes();
     var m;
-    var expr;
+    var expr = undefined;
     if ((m = s.match(/\(\):* (.+)/)) != undefined) {
       expr = m[1];
     }
     if (expr != undefined) {
-      FIELDS.put("name", cmd + " ->" + m[1]);
+      if (
+        expr.includes("[__.savetmpfiles]") ||
+        expr.includes("[__.savetmpfilesmode]")
+      ) {
+        IGNORE_RECORD = true;
+      } else {
+        FIELDS.put("name", cmd + " ->" + expr);
+      }
     }
 
     break;
   }
 
   default:
-    var param1=RECORD.getField("param1");
-    var param2=RECORD.getField("param2");
-    FIELDS.put("name", cmd + (param1?' '+param1:'')+ (param2?' '+param2:''));
+    var param1 = RECORD.getField("param1");
+    var param2 = RECORD.getField("param2");
+    FIELDS.put(
+      "name",
+      cmd + (param1 ? " " + param1 : "") + (param2 ? " " + param2 : "")
+    );
     break;
 }
