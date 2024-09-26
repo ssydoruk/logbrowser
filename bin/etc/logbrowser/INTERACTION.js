c72fb5466533c1a9741b376cb02461308448540f
Stepan Sydoruk <ssydoruk@apple.com>
2024-09-26 15:45:23 -0500

    ORSM display sid

---
+++ bin/etc/logbrowser/INTERACTION.js
@@ -0,0 +1,13 @@
+
+// uncomment when debuging standalone
+// const dbg=require('./DEBUG.js'); var RECORD=dbg.RECORD;  var FIELDS = dbg.FIELDS;
+
+for (var s of RECORD.getBytes().split('\n')) {
+    if ((m = s.match(/attr_strategy_id.+= \"(.+)\"$/i)) != undefined) {
+        FIELDS.put("strategy1", m[1]);
+    }
+    else if ((m = s.match(/\bSession\b.+=\s\"([^\"]+)/)) != undefined) {
+        FIELDS.put("sid", "sid=" + m[1]);
+    }
+}
+// console.log(FIELDS.entrySet());
--- bin/etc/logbrowser/ORSM.js
+++ bin/etc/logbrowser/ORSM.js
@@ -1,111 +1,147 @@
+// FIELDS.put("name", RECORD.getField("metric"));
 switch (RECORD.getField("metric")) {
-    case "eval_expr": {
-      var s = RECORD.getBytes();
-      var m;
-      var expr;
-      if ((m = s.match(/expression='([^\']+)' /)) != undefined) {
-        expr = m[1];
-      }
-      if (expr != undefined) {
-        if (
-          expr.startsWith("storeEvent") ||
-          expr.startsWith("storeUnhandledEvent") 
-        ) {
-            IGNORE_RECORD = true;
-        } else {
-            var type;
-            var result;
-  
-            if ((m = s.match(/type=[']([^\']+)' /)) != undefined) {
-                type= m[1];
-            }
-            if ((m = s.match(/result='([^\']+)' /)) != undefined) {
-                result=m[1];
-            }
-            if(result != undefined && result.endsWith('.script.done') ){
-                IGNORE_RECORD = true;
-            }
-            else {
-                FIELDS.put("param2", "result:" + result);
-                FIELDS.put("param1", "type:" + type);
-                FIELDS.put("param", expr);
-            }
+  case "eval_expr": {
+    var s = RECORD.getBytes();
+    var m;
+    var expr;
+    if ((m = s.match(/expression='([^\']+)' /)) != undefined) {
+      expr = m[1];
+    }
+    if (expr != undefined) {
+      if (
+        expr.startsWith("storeEvent") ||
+        expr.startsWith("storeUnhandledEvent")
+      ) {
+        IGNORE_RECORD = true;
+      } else {
+        var type;
+        var result;
+
+        if ((m = s.match(/type=[']([^\']+)' /)) != undefined) {
+          type = m[1];
         }
-      }
-      break;
-    }
-  
-    case "eval_condition": {
-      var s = RECORD.getBytes();
-      var m;
-      if ((m = s.match(/condition='(.+)' result='([^\']+)' /)) != undefined) {
-        FIELDS.put("param", m[1]);
-        FIELDS.put("param1", "result:" + m[2]);
-      }
-      break;
-    }
-  
-    case "transition": {
-      var s = RECORD.getBytes();
-      var m;
-      if ((m = s.match(/name='([^\']+)' event='([^\']+)'/)) != undefined) {
-        FIELDS.put("param", m[1]);
-        var p1 = m[2];
-        FIELDS.put("param1", m[2]);
-        if (p1 != "*") {
-          if ((m = s.match(/target='([^\']+)' /)) != undefined) {
-            FIELDS.put("param2", m[1]);
-          }
+        if ((m = s.match(/result='([^\']+)' /)) != undefined) {
+          result = m[1];
         }
-      }
-      break;
-    }
-  
-    case "log": {
-      var s = RECORD.getBytes();
-      var m;
-      if ((m = s.match(/expr='(.+)' lab/)) != undefined) {
-        var p = m[1];
-        if (
-          p.startsWith("done.state.") ||
-          p.startsWith("Running ") ||
-          p.startsWith("RelativePathURL =") ||
-          p.startsWith("Entry1.done event") ||
-          p.endsWith("End of this session") ||
-          p.startsWith("return values = (") ||
-          p.includes("script.done event") ||
-          p.includes("event received in user application thread")
-        ) {
+        if (result != undefined && result.endsWith('.script.done')) {
           IGNORE_RECORD = true;
-        } else {
-          FIELDS.put("param", p);
+        }
+        else {
+          FIELDS.put("param2", "result:" + result);
+          FIELDS.put("param1", "type:" + type);
+          FIELDS.put("param", expr);
         }
       }
-      break;
     }
-  
-    case "onentry":
-    case "state_enter":
-    case "state_exit":
-    case "event_queued":
-    case "event_processed":
-    case "onexit": {
-      var m;
-      if ((m = RECORD.getBytes().match(/name='([^\']+)' /)) != undefined) {
-        FIELDS.put("param", m[1]);
-      }
-      break;
-    }
-  
-    case "fm_exec_error":
-    case "exec_error": {
-      var m;
-      if ((m = RECORD.getBytes().match(/error='([^\']+)' /)) != undefined) {
-        FIELDS.put("param", m[1]);
-      }
-      break;
-    }
-  
-    default:
-      break;
+    break;
   }
+
+  case "eval_condition": {
+    var s = RECORD.getBytes();
+    var m;
+    if ((m = s.match(/condition='(.+)' result='([^\']+)' /)) != undefined) {
+      FIELDS.put("param", m[1]);
+      FIELDS.put("param1", "result:" + m[2]);
+    }
+    break;
+  }
+
+  case "transition": {
+    var s = RECORD.getBytes();
+    var m;
+
+    var name1 = undefined, evt = undefined, target = undefined, cond = undefined;
+
+    if ((m = s.match(/name='([^\']+)'/)) != undefined) {
+      name1 = m[1];
+    }
+    if ((m = s.match(/cond='([^\']+)'/)) != undefined) {
+      cond = m[1];
+
+    }
+    if ((m = s.match(/event='([^\']+)'/)) != undefined) {
+      evt = m[1];
+    }
+    if ((m = s.match(/target='([^\']+)'/)) != undefined) {
+      target = m[1];
+    }
+    if (evt == '*' && !target)
+      IGNORE_RECORD = true;
+    else {
+      if (name1)
+        FIELDS.put("param1", name1);
+      if (cond)
+        FIELDS.put("cond", { value: "cond:" + cond, hidden: true });
+      if (evt && !evt.endsWith('.script.done'))
+        FIELDS.put("event", "evt:" + evt);
+      if (target)
+        FIELDS.put("target", "targ:" + target);
+    }
+    break;
+  }
+
+  case "log": {
+    var s = RECORD.getBytes();
+    var m;
+    if ((m = s.match(/expr='(.+)' lab/)) != undefined) {
+      var p = m[1];
+      if (
+        p.startsWith("done.state.") ||
+        p.startsWith("Running ") ||
+        p.startsWith("RelativePathURL =") ||
+        p.startsWith("Entry1.done event") ||
+        p.endsWith("End of this session") ||
+        p.endsWith(".script.done") ||
+        p.startsWith("return values = (") ||
+        p.includes("script.done event") ||
+        p.includes("event received in user application thread")
+      ) {
+        IGNORE_RECORD = true;
+      } else {
+        FIELDS.put("param", p);
+      }
+    }
+    break;
+  }
+
+  case "onentry":
+  case "state_enter":
+  case "state_exit":
+  case "onexit": {
+    var m;
+    if ((m = RECORD.getBytes().match(/name='([^\']+)'/)) != undefined) {
+      FIELDS.put("param", m[1]);
+    }
+    break;
+  }
+
+  case "event_queued":
+  case "event_processed": {
+    // console.log('-----'+RECORD.getField("metric"));
+    var m;
+    if ((m = RECORD.getBytes().match(/name='([^\']+)'/)) != undefined) {
+      if (m[1].endsWith('.script.done') || m[1].search(/Entry_.+\.done/) >= 0 || m[1].search(/done.state\..+_entry/) >= 0) {
+        // console.log('ignored: '+m[1]);
+        IGNORE_RECORD = true;
+      }
+      else {
+        // console.log('put: '+m[1]);
+        FIELDS.put("param", m[1]);
+      }
+    }
+    break;
+  }
+
+
+  case "fm_exec_error":
+  case "exec_error": {
+    var m;
+    if ((m = RECORD.getBytes().match(/error='([^\']+)' /)) != undefined) {
+      FIELDS.put("param", m[1]);
+    }
+    break;
+  }
+
+  default:
+    break;
+}
